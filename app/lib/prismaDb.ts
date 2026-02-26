507956f7e74a4dce5a00c7c96ff82b19ae6d8895
Rahmanhusain <rahmanhusain899@gmail.com>
2026-02-27 01:14:28 +0530

    abcd rahman

--- app/lib/prismaDb.ts
+++ app/lib/prismaDb.ts
@@ -1,13 +1,18 @@
-import "dotenv/config";
 import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
+
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
-  const adapter = new PrismaPg({
+
+const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 })
-export const prisma =
-  globalForPrisma.prisma ??  new PrismaClient({adapter,})
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+
+prisma.$connect()
+  .then(() => console.log('✅ Connected to database'))
+  .catch((e: unknown) => console.error('❌ Failed to connect to database:', e))
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
